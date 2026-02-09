06da9fd7e5ec0381010fbaec0c7e4c04e8fbbb8d
lava slasher <hsn@njit.edu>
2026-02-08 20:56:59 -0500

    Fix logic and finalize GCD function

---
+++ students_submissions/gcd_hsn.py
@@ -0,0 +1,26 @@
+def gcd(a: int, b: int) -> int:
+    #if not (a, int) or not (b, int):
+    #print("Error: needs two integer inputs to work correctly")
+    #return None
+    a = abs(a)
+    b = abs(b)
+    #if a = 0
+
+    #if abs(a) == 0
+    if b == 0:
+        return a
+
+
+
+
+
+    c = a % b
+    d = b % a
+    gcdd = (b, c)
+    return gcdd
+
+
+    #Test Cases from hw assignment
+print(gcd(54, 24)) 
+print(gcd(48, 18))  
+print(gcd(101, 10)) 
